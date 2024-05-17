631d12daec8115fbc2fcbb09992f69e54e27b204
Rafal <rafek131@wp.pl>
2024-05-17 11:54:15 +0200

    'give likes' exercise (lesson 4) completed

--- src/routes/Posts.tsx
+++ src/routes/Posts.tsx
@@ -1,20 +1,46 @@
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { arrayRemove, arrayUnion, collection, doc, getDocs, onSnapshot, query, updateDoc } from 'firebase/firestore';
+import { useAuth } from '../providers/AuthProvider';
 import { db } from '../main';
 import './Posts.styles.scss';
 
 export const Posts = () => {
 	const [posts, setPosts] = useState<any>([]);
+	const { user } = useAuth();
+
+	const handleAddLike = async (id: any) => {
+		try {
+			const postRef = doc(db, 'posts', id);
+			await updateDoc(postRef, {
+				likes: arrayUnion(user.uid),
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleRemoveLike = async (id: any) => {
+		try {
+			const postRef = doc(db, 'posts', id);
+			await updateDoc(postRef, {
+				likes: arrayRemove(user.uid),
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	useEffect(() => {
-		(async () => {
-			const posts = await getDocs(collection(db, 'posts'));
-			const postsData = posts.docs.map(post => ({
+		const postsQuery = query(collection(db, 'posts'));
+		const unsub = onSnapshot(postsQuery, postsSnapshot => {
+			const postsData = postsSnapshot.docs.map(post => ({
 				id: post.id,
 				...post.data(),
 			}));
 			setPosts(postsData);
-		})();
+		});
+
+		return () => unsub();
 	}, []);
 
 	return (
@@ -23,6 +49,15 @@ export const Posts = () => {
 				<div className='post' key={post.id}>
 					<h2>{post.title}</h2>
 					<p>{post.content}</p>
+					{post.likes.length && post.likes.includes(user.uid) ? (
+						<button className='posts__button' onClick={() => handleRemoveLike(post.id)}>
+							{post.likes.length} 🤍 Unlike
+						</button>
+					) : (
+						<button className='posts__button' onClick={() => handleAddLike(post.id)}>
+							{post.likes.length} 💖 Like
+						</button>
+					)}
 				</div>
 			))}
 		</div>
