839f2439ed571c7510c4a7a9716d4c4d60c8c657
Rafal <rafek131@wp.pl>
2024-05-20 12:54:20 +0200

    authorization - step1 (authorization works, no react-router)

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,46 @@
+import { onAuthStateChanged, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
+import { auth, provider } from './main';
+
 export const Root = () => {
+	const [user, setUser] = useState<any>(null);
+
+	onAuthStateChanged(auth, user => {
+		if (user) {
+			setUser(user);
+		} else {
+			setUser(null);
+		}
+	});
+
+	const handleSignIn = async () => {
+		try {
+			await signInWithPopup(auth, provider);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleSignOut = async () => {
+		try {
+			await signOut(auth);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
 	return (
 		<div>
-			<h1>Hello world!</h1>
+			<h1>Hello {user ? user.displayName : 'world!'}</h1>
+			<button onClick={handleSignIn}>Sign in</button>
+			<button onClick={handleSignOut}>Sign out</button>
+			{user ? (
+				<div>
+					<h2>Posts</h2>
+					<h3>Lorem ispum</h3>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+				</div>
+			) : null}
 		</div>
 	);
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from 'firebase/app';
+import { GoogleAuthProvider, getAuth } from 'firebase/auth';
 import { Root } from './Root';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyDEhSy0p9_Z1ZdIQllJLLmgJChA7pqoFz0',
+	authDomain: 'fir-test-2105a.firebaseapp.com',
+	projectId: 'fir-test-2105a',
+	storageBucket: 'fir-test-2105a.appspot.com',
+	messagingSenderId: '420600315772',
+	appId: '1:420600315772:web:51362e5b6c88ad63a3aef9',
+};
+
+const app = initializeApp(firebaseConfig);
+export const provider = new GoogleAuthProvider();
+export const auth = getAuth(app);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
