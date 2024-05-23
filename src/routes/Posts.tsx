a7758f25d968d2015de9bb87910e8ad80e3de9d8
Rafal <rafek131@wp.pl>
2024-05-23 11:49:51 +0200

    posts likes - completed

--- src/routes/Posts.tsx
+++ src/routes/Posts.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { collection, getDocs } from 'firebase/firestore';
+import { useAuth } from '../providers/AuthProvider';
+import { arrayRemove, arrayUnion, collection, doc, getDocs, onSnapshot, query, updateDoc } from 'firebase/firestore';
 import { db } from '../main';
 import './Posts.styles.scss';
 
@@ -18,16 +19,56 @@ type postType = {
 
 export const Posts = () => {
 	const [posts, setPosts] = useState<postType[] | never[]>([]);
+	const { user } = useAuth();
+
+	const handleAddLike = async (postId: string) => {
+		const postRef = doc(db, 'posts', postId);
+		try {
+			await updateDoc(postRef, {
+				likes: arrayUnion(user?.uid),
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const handleRemoveLike = async (postId: string) => {
+		const postRef = doc(db, 'posts', postId);
+		try {
+			await updateDoc(postRef, {
+				likes: arrayRemove(user?.uid),
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	// useEffect(() => {
+	// 	(async () => {
+	// 		try {
+	// 			const posts = await getDocs(collection(db, 'posts'));
+	// 			const postsData = posts.docs.map(post => ({
+	// 				id: post.id,
+	// 				...(post.data() as postDataType),
+	// 			}));
+	// 			setPosts(postsData);
+	// 		} catch (error) {
+	// 			console.log(error);
+	// 		}
+	// 	})();
+	// }, []);
 
 	useEffect(() => {
-		(async () => {
-			const posts = await getDocs(collection(db, 'posts'));
-			const postsData = posts.docs.map(post => ({
+		const postsQuery = query(collection(db, 'posts'));
+		const unsub = onSnapshot(postsQuery, postsSnapshot => {
+			const postsData = postsSnapshot.docs.map(post => ({
 				id: post.id,
 				...(post.data() as postDataType),
 			}));
 			setPosts(postsData);
-		})();
+		});
+
+		return () => unsub();
 	}, []);
 
 	return (
@@ -37,6 +78,15 @@ export const Posts = () => {
 						<div className='post' key={post.id}>
 							<h2>{post.title}</h2>
 							<p>{post.content}</p>
+							{post.likes.length && post.likes.includes(user!.uid) ? (
+								<button className='posts__button' onClick={() => handleRemoveLike(post.id)}>
+									{post.likes.length} 💖 Unlike
+								</button>
+							) : (
+								<button className='posts__button' onClick={() => handleAddLike(post.id)}>
+									{post.likes.length} 🤍 Like
+								</button>
+							)}
 						</div>
 				  ))
 				: null}
